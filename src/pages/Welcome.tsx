f2938fcdea329dc210e500206c050ca7fc2c49fe
Rishabh <joshir838@gmail.com>
2026-01-02 20:59:01 +0530

    welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,10 +1,11 @@
 export default function Welcome() {
   return (
-    <div className="flex w-full h-full justify-center items-center">
-      <h1 className="text-lg sm:text-2xl md:text-4xl lowercase font-black text-center mb-5">
-        welcome to <span className="text-amber-500">Zen</span>
+    <div className="flex w-full h-full justify-center items-center flex-col">
+      <h1 className="text-lg sm:text-2xl md:text-4xl  font-black text-center mb-2">
+        Welcome to <span className="text-amber-500">zen</span>
         ban
       </h1>
+      <p className="text-stone-500">Please create a new board</p>
     </div>
   );
 }
