854b3bded4faf32c9f930181ee212443c601002f
Rishabh <joshir838@gmail.com>
2026-01-28 13:30:38 +0530

    Page not found ui update

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -1,6 +1,6 @@
 export default function Columns({ children }: { children: React.ReactNode }) {
   return (
-    <div className="md:h-1/3 lg:h-full flex-1 min-w-60 bg-columns flex-wrap">
+    <div className="min-h-[15%] md:min-h-1/3 lg:h-full flex-1 min-w-60 bg-columns flex-wrap">
       {children}
     </div>
   );
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,3 +1,17 @@
+import { Home } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center flex-col gap-5 overflow-hidden bg-background-900">
+      <p className="text-2xl font-bold">Page Not Found</p>
+      <Link
+        to={"/"}
+        className="flex flex-col justify-center items-center border p-5 rounded-full hover:text-accent transition-all duration-300"
+      >
+        <Home className="size-10" />
+        Go Home
+      </Link>
+    </div>
+  );
 }
